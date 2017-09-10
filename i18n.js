e08b83c7786319f59133a9099c48e803fc6bf9d9
Wolfgang Marcos <wolfgang@alcateia.eti.br>
2017-09-09 21:20:16 -0400

    Adiciona suporte básico à i18n

---
+++ i18n.js
@@ -0,0 +1,60 @@
+const messages = {
+    pt_BR: {
+        description: "PT_BR - Português do Brasil",
+        message: {
+            locale_picker: {
+                txt_title: "Locale",
+                txt_description: "Locale usado na interface e nas consultas"
+            },
+            search_panel: {
+                txt_title:      "Consulta",
+                txt_keyword:    "Palavra chave",
+                btn_search:     "Pesquisar",
+                txt_searching:  "Pesquisando..."
+            },
+            result_panel: {
+                // Pluralization format: String for 0 results | String for one result | String for 2 or more results
+                txt_title:  "Nenhum resultado para '{keyword}' | Um resultado para '{keyword}' | {count} resultados para '{keyword}'"
+            },
+            result_table_header: {
+                txt_title:      "Título",
+                txt_summary:    "Resumo",
+                txt_options:    "Opções"
+            },
+            result_table_row: {
+                txt_action_view:        "Visualizar documento no Kitsune",
+                txt_action_edit:        "Editar documento no Kitsune",
+                txt_action_download:    "Download do documento como HTML"
+            }
+        }
+    },
+    en_US: {
+        description: "EN_US - North American English",
+        message: {
+            locale_picker: {
+                txt_title: "Locale",
+                txt_description: "Locale used in interface and search"
+            },
+            search_panel: {
+                txt_title:          "Search",
+                txt_keyword:        "Keyword",
+                btn_search:         "Search",
+                txt_searching:      "Searching..."
+            },
+            result_panel: {
+                // Pluralization format: String for 0 results | String for one result | String for 2 or more results
+                txt_title:  "No results for '{keyword}' | Single result for '{keyword}' | {count} results for '{keyword}'"
+            },
+            result_table_header: {
+                txt_title:      "Title",
+                txt_summary:    "Summary",
+                txt_options:    "Options"
+            },
+            result_table_row: {
+                txt_action_view:        "View document on Kitsune",
+                txt_action_edit:        "Edit document on Kitsune",
+                txt_action_download:    "Download document as HTML"
+            }
+        }
+    }
+};
